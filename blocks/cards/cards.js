3dffc5ba2a2a0455cf1645928081b9b3b6b6f794
surendra <nsurendra@pkglobal.com>
2024-08-19 15:33:24 +0530

    Updating video

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,6 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import { decorateVideoBlock } from '../video/video.js';
 
 export default function decorate(block) {
   /* change to ul, li */
@@ -9,7 +10,12 @@ export default function decorate(block) {
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      }
+      else if(div.querySelector('a')){
+        decorateVideoBlock(div);
+      }
       else div.className = 'cards-card-body';
     });
     ul.append(li);
