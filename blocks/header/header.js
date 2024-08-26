34c4dafaaf99204a7d6c88258c5f9b791b3d18ce
surendra <nsurendra@pkglobal.com>
2024-08-26 17:24:44 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -169,8 +169,8 @@ if (navSections) {
     });
 
     // Create and append the headerShadowCard div
-    const headerShadowCard = document.createElement('div'); // Renamed the variable
-     headerShadowCard.className = 'header-shadow-card';
+    //const headerShadowCard = document.createElement('div'); // Renamed the variable
+    // headerShadowCard.className = 'header-shadow-card';
     // headerShadowCard.innerHTML = '<p>This is the header shadow card content.</p>';
     // Replace with your actual content
     // headerShadowCard.style.display = 'none';
@@ -181,7 +181,7 @@ if (navSections) {
     navSection.addEventListener('mouseenter', () => {
       if (isDesktop.matches) {
         const content = generateList(navConfig[navTitle]);
-        headerShadowCard.appendChild(content);
+        const headerShadowCard = navSection.appendChild(content);
         headerShadowCard.style.display = 'block';
       }
     });
