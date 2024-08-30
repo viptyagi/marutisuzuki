322c9e3f336f1a431a36c0412ec17819b1678498
Ashish Raj <ashish.raj@concentrix.com>
2024-08-30 13:11:17 +0530

    Update nexa-cars.js

--- blocks/nexa-cars/nexa-cars.js
+++ blocks/nexa-cars/nexa-cars.js
@@ -2,7 +2,7 @@ console.log('nexa-cars.js loading...');
 
 export default function decorate(block) {
   const div = document.querySelector('main > div.section.nexa-cars');
-  const columns = [...div.children][1];
+  const columns = div.children[1];
   const grid = columns.children[0].children[0];
   [...grid.children].forEach((row) => {
       row[0].className = 'price';
