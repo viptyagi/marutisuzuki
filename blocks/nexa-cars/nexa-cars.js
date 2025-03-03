a6aa9dbccc21083793a69ff929c48909095c442e
venkatanarayanbattula <94977341+venkatanarayanbattula@users.noreply.github.com>
2025-03-03 18:17:27 +0530

    Update nexa-cars.js

--- blocks/nexa-cars/nexa-cars.js
+++ blocks/nexa-cars/nexa-cars.js
@@ -1,29 +1,17 @@
-/* eslint-disable no-console */
 console.log('nexa-cars.js loading...');
 
-export default function decorate() {
+export default function decorate(block) {
   const div = document.querySelector('main > div.section.nexa-cars');
-  if (!div) return;
-
   const columns = div.children[1];
-  if (!columns) return;
-
-  const grid = columns.children[0]?.children[0];
-  if (!grid) return;
-
+  const grid = columns.children[0].children[0];
   grid.className = 'grid';
-
   [...grid.children].forEach((row) => {
-    if (row.children.length < 6) {
-      return;
-    }
-
-    row.className = 'car';
-    row.children[0].className = 'modelImage';
-    row.children[1].className = 'price';
-    row.children[2].className = 'showroom';
-    row.children[3].className = 'modelName';
-    row.children[4].className = 'downloadIcon';
-    row.children[5].className = 'downloadBrochure';
+      row.className = 'car';
+      row.children[0].className = 'modelImage';
+      row.children[1].className = 'price';
+      row.children[2].className = 'showroom';
+      row.children[3].className = 'modelName';
+      row.children[4].className = 'downloadIcon';
+      row.children[5].className = 'downloadBrochure';
   });
 }
